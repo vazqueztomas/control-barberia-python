b6ce0cca9b07f0270aec278455fcf9392f2f14a6
Tomas Vazquez <vazquezt2018@gmail.com>
2023-09-12 08:08:52 -0300

    add/ get total and search by date

--- main.py
+++ main.py
@@ -2,8 +2,47 @@ from database import create_database_connection
 from decouple import config
 from mysql.connector import Error
 from datetime import datetime
+import os
 
 connection = create_database_connection(config('DATABASE_HOSTNAME'), config('DATABASE_USERNAME'), config('DATABASE_PASSWORD'), 'barber')
+def menu_principal():
+  print("Seleccione una opcion: \n")
+  print("1) Agregar nuevo corte")
+  print("2) Ver todos los cortes (con toda la info)")
+  print("3) Ver corte según día")
+  print("4) Ver total ganado")
+  print("0) Salir")
+  print()
+  opcion = int(input("Ingrese la opción: "))
+
+  while(opcion != 0):
+    if opcion == 1:
+      add_corte()
+    elif opcion == 2:
+      get_all_cortes()
+    elif opcion == 3:
+      fecha = input("Ingrese una fecha (formato YYYY-MM-dd): ")
+      search_by_date(fecha)
+    elif opcion == 4:
+      total = get_total_ganado()
+      print(f"El sistema lleva facturando {total} ARS.\n")
+    elif opcion == 0:
+      print("Gracias por utilizar el sistema de barbería")
+      os.system("clear")
+      break
+    else:
+      print("Opción incorrecta.")
+    
+    opcion = int(input("Ingrese una opción: "))
+
+
+
+def main():
+  os.system('clear')
+  print("Bienvenido al sistema de control de barbería")
+  print()
+  menu_principal()
+
 
 def execute_query(connection, query):
   cursor = connection.cursor()
@@ -33,16 +72,15 @@ def add_corte():
   fecha = str(fecha)
   
   increment_id = read_query(connection, "select id from cortes")
-  increment_id = increment_id + 1
+  increment_id = increment_id[-1][0] + 1
 
-
+  print(increment_id)
   add_query = f"""
   INSERT INTO cortes
   VALUES ({increment_id},{valor},'2023-09-12')
   """
   
   execute_query(connection, add_query)
-  print('add succesfull')
 
 def get_all_cortes():
   results = read_query(connection, "select * from cortes")
@@ -61,8 +99,16 @@ def get_cantidad_cortes(fecha):
   cantidad_cortes = read_query(connection, f"select id from cortes where fecha ='{fecha}'")
   print(f'El dia {fecha} se realizaron {cantidad_cortes[-1][0]} cortes')
 
+def get_total_ganado():
+  cantidad_cortes = read_query(connection, f"select valor from cortes")
+  total = 0
 
-search_by_date('2023-09-11')
-get_cantidad_cortes('2023-09-11')
+  for corte in cantidad_cortes:
+    total = total + corte[0]
+
+  return total
+
+
+main()
 
 
